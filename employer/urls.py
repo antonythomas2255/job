dbe9f9f9d8c82722c06742509332f14270566a44
Antony Thomas <antonythomas2255@gmail.com>
2022-06-06 19:15:46 +0530

    Initial commit

---
+++ employer/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from employer import views
+urlpatterns = [
+    path('home',views.EmployerHomeView.as_view(),name="emp-home"),
+    path('jobs/add',views.AddJobView.as_view(),name="emp-addjob"),
+    path('jobs/all',views.ListJobView.as_view(),name="emp-alljobs"),
+    path('jobs/detail/<int:id>',views.JobDetailView.as_view(),name="emp-jobdetail"),
+    path('jobs/change/<int:id>',views.JobEditView.as_view(),name="emp-editjob"),
+    path('jobs/remove/<int:id>', views.JobDeleteView.as_view(), name="emp-deletejob"),
+    path('users/accounts/signup',views.SignUpView.as_view(),name="signup"),
+    path('users/accounts/signin', views.SignInView.as_view(), name="signin"),
+    path('users/accounts/signout',views.signout_view,name="signout"),
+    path('users/accounts/change', views.ChangePassword.as_view(), name="password-change"),
+    path('users/accounts/reset', views.PasswordResetView.as_view(), name="password-reset"),
+
+]
+
